9491088b8ae5c8c13c5a823827c55cfaba1c648b
raj.singh2005 <raj.singh2005.dev@gmail.com>
2026-07-24 03:23:35 +0530

    Add dbConnect function for MongoDB connection management

---
+++ src/lib/dbConnect.ts
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+type ConnectionObject = {
+  isConnected?: number;
+};
+
+const connection: ConnectionObject = {};
+
+async function dbConnect(): Promise<void> {
+  if (connection.isConnected) {
+    console.log("database already connected");
+    return;
+  }
+
+  try {
+    const db = await mongoose.connect(
+      (process.env.MONGODB_URI as string) || "",
+    );
+    console.log(db);
+    console.log(db.connections);
+
+    connection.isConnected = db.connections[0].readyState;
+    console.log("DB connected successfully");
+  } catch (error) {
+    console.log("DB connection failed", error);
+
+    process.exit(1);
+  }
+}
+
+export default dbConnect;
